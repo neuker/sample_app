2cf11464d5b2b84efb99cc3fc98d249184061389
Norm Euker <admin@njedesign.com>
2010-11-17 15:51:06 -0500

    Working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      # success
+      sign_in user
+      redirect_to user
     end
     
   end
